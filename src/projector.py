de8b1f2c325dc40784c53c7c3ba58b4caec92693
devashishdubal <dubaldevashish@gmail.com>
2023-09-03 21:47:41 +0530

    added basic classes,functions for Point, Camera

--- src/projector.py
+++ src/projector.py
@@ -1,1 +1,35 @@
-print("arif is the best")
+class Point:
+    x = 0
+    y = 0
+    z = 0
+    #r = 0
+    colour = "red"
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.colour = "red"
+        
+        
+
+
+
+
+class Camera:
+    x = 0
+    y = 0
+    z = 0
+    focal_length = 0
+    def _init_(self,x,y,z,f):
+        self.x = x
+        self.y = y
+        self.z = z
+        self.focal_length = f
+
+
+    def retProjected(self,p1):
+        xp = self.focal_length * (p1.x-self.x) / (self.focal_length + p1.z-self.z)
+        yp = self.focal_length * (p1.y-self.y) / (self.focal_length + p1.z-self.z)
+
+        
+        return xp,yp
